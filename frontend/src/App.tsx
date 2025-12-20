adf0f8236e28f5f25bb2232c4a66a3e6eb8d445b
Tetiana Klitna <tetiana@artcraft.studio>
2025-12-19 20:54:05 -0800

    proof prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [error, setError] = useState(null);
 
-const     c={b:1}
-console.log(    c )
-
+  const m = { b: 1 };
+  console.log(m);
 
   useEffect(() => {
     // Call the backend API
-    fetch('http://localhost:8080/api/hello')
+    fetch("http://localhost:8080/api/hello")
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Failed to fetch from backend');
+          throw new Error("Failed to fetch from backend");
         }
         return response.json();
       })
@@ -27,9 +26,9 @@ console.log(    c )
   }, []);
 
   return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Frontend ↔ Backend Test</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
       {!error && (
         <p>
           Message from API: <strong>{message}</strong>
@@ -39,4 +38,4 @@ console.log(    c )
   );
 }
 
-export default App
+export default App;
