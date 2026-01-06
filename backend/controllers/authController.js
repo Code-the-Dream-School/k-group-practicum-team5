9c43452c47cc7acc5aa654b460195669876d66c8
Maher <YOUR_EMAIL_ADDRESS>
2026-01-06 00:43:28 -0800

    Add http-status-codes library and use it in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const StatusCodes  =  require('http-status-codes');
 
 const hashPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
@@ -9,13 +10,13 @@ const hashPassword = async (password) => {
 
 const register = async (req, res) => {
     if (req.body.password.length < 6) {
-        return res.status(400).json({ msg: 'Password must be at least 6 characters long' });
+        return res.status(StatusCodes.BAD_REQUEST).json({ msg: 'Password must be at least 6 characters long' });
     }
     const hashedPassword = await hashPassword(req.body.password);
     req.body.password = hashedPassword;
 
     const user = await User.create(req.body);
-    res.status(201).json({ msg: 'user created' });
+    res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 module.exports = { register };
