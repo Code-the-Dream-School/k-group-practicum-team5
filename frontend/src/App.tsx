270a3d73f7f84fc60927df1f6c413fec382164ac
MASOUMA2023 <ahmadimasooma12@gmail.com>
2026-01-09 10:39:52 -0500

    Fix App.tsx and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,49 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainLayout from "@/layout/MainLayout";
 import VideoListPage from "@/pages/VideoListPage";
+import Contact from "@/pages/Contact";
 import { Box } from "@mui/material";
 
 function App() {
-  // const [message, setMessage] = useState("");
-  // const [error, setError] = useState(null);
-  // useEffect(() => {
-  //   // Call the backend API
-  //   fetch("http://localhost:5000/api/hello")
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Failed to fetch from backend");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setMessage(data.message);
-  //     })
-  //     .catch((err) => {
-  //       setError(err.message);
-  //     });
-  // }, []);
-
-  return (
-    // <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-    //   <h1>Frontend ↔ Backend Test</h1>
-    //   {error && <p style={{ color: "red" }}>{error}</p>}
-    //   {!error && (
-    //     <p>
-    //       Message from API: <strong>{message}</strong>
-    //     </p>
-    //   )}
-    //   <FormattedLabel text="MUI is working" />
-    //   MUI is working <CheckCircleIcon />
-    // </main>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Box>Home Page</Box>} />
           <Route path="video" element={<VideoListPage />} />
+          <Route path="contact" element={<Contact />} />
           <Route path="*" element={<p>Page Not Found</p>} />
         </Route>
       </Routes>
