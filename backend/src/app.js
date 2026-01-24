4a9cd35fd19cedb3966f8fbda63e39b4a1019d07
Tetiana Klitna <tetiana@artcraft.studio>
2026-01-23 23:52:33 -0800

    revert

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,8 +13,7 @@ const adminRoutes = require('./routes/adminRouter');
 const contactInfoRoutes = require('./routes/contactInfo.routes')
 const volunteeringRoutes = require('./routes/volunteeringRouter');
 const businessHoursRoutes = require('./routes/businessHours.routes')
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
@@ -50,8 +49,5 @@ app.use('/api/v1/business-hours', businessHoursRoutes)
 app.get('/', (req, res) => {
   res.send('Backend API is running');
 });
-
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 module.exports = app;
--- backend/src/routes/upload.routes.js
+++ backend/src/routes/upload.routes.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const uploadController = require('../controllers/upload.controller'); 
 const multer = require('../middleware/upload.multer'); 
 
-const auth = require('../middleware/auth');
-const admin = require('../middleware/admin');
-
+router.post('/upload', multer.single('image'), uploadController.uploadImage);
 router.get('/', uploadController.getImages);
-
-router.post('/upload', auth, admin, multer.single('image'), uploadController.uploadImage);
-router.delete('/', auth, admin, uploadController.deleteImage);
-router.put('/', auth, admin, uploadController.updateImage);
 
 module.exports = router;
